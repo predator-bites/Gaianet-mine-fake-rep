956be96b131e0698c3f8878f34da8026233a7617
predator-bites <142865899+predator-bites@users.noreply.github.com>
2025-01-21 00:57:17 +0200

    Update gaia_install_1.sh

--- gaia_install_1.sh
+++ gaia_install_1.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Цветовые коды
+RED='\033[31m'
+BLUE='\033[34m'
+RESET='\033[0m'
+
+# Логотип
+logo() {
+    echo -e "
+${RED}  ____   ${BLUE}____  
+${RED} |  _ \\  ${BLUE}|  _ \\ 
+${RED} | | | | ${BLUE}| |_) |
+${RED} | |_| | ${BLUE}|  __/ 
+${RED} |____/  ${BLUE}|_|    
+${RESET}
+"
+}
+
+# Вызов логотипа
+logo
+
+# Основная логика
+echo "Welcome to DropPredator!"
+
 # Скрипт автоустановки ноды Gaianet для Ubuntu 22.04
 
 set -e
